875f9923eb4066fb1627b1da0f1462bea2b80aea
Teddy Ward <teddy@kevalaanalytics.com>
2021-03-24 18:14:08 -0700

    some sample pages

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
+app = express()
+app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.all('/**', function (req, res) {
+    req.url = req.url + '.html';
+    app.handle(req, res);
+});
